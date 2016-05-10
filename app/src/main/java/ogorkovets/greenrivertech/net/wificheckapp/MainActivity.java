2839f3b653165cfc2d13aa61e311d32b9ad4b02c
OGorkovets <OGorkovets@mail.greenriver.edu>
2016-05-09 23:16:22 -0700

    second activity added

--- app/src/main/java/ogorkovets/greenrivertech/net/wificheckapp/MainActivity.java
+++ app/src/main/java/ogorkovets/greenrivertech/net/wificheckapp/MainActivity.java
@@ -7,9 +7,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,6 +23,8 @@ public class MainActivity extends Activity {
 
         //Initialize Watch Video Button
         Button btnSrc = (Button) findViewById(R.id.watchvideo);
+
+        Button refresh = (Button) findViewById(R.id.button);
 
         TextView tv2 = (TextView) findViewById(R.id.textWiFi);
 
@@ -51,29 +51,32 @@ public class MainActivity extends Activity {
                             Toast.LENGTH_LONG).show();
                 }
             });
-
         }
 
-
+        refresh.setBackgroundColor(Color.YELLOW);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                finish();
+                startActivity(getIntent());
+            }
+        });
     }
 
     public boolean isNetworkOnline() {
         boolean status=false;
         try{
             ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInfo = cm.getNetworkInfo(0);
-            if (netInfo != null && netInfo.getState()==NetworkInfo.State.CONNECTED) {
+            //NetworkInfo netInfo = cm.getNetworkInfo(0);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
                 status= true;
             }else {
-                netInfo = cm.getNetworkInfo(1);
-                if(netInfo!=null && netInfo.getState()== NetworkInfo.State.DISCONNECTED)
-                    status= false;
+                status= false;
             }
         }catch(Exception e){
             e.printStackTrace();
             return false;
         }
         return status;
-
     }
 }
